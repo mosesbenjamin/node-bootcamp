c5d659e6f49167e0c53b48718ffbc8ce412b07a4
mosesbenjamin <mavewreek@gmail.com>
2020-11-17 00:25:55 +0100

    Creating a simple web server and routing

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const fs = require('fs');
+const http = require('http');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+////////////////////////////////////////////////////
+// SERVER
 
-const textOut = `This is what we know for now ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!');
+const server = http.createServer((req, res)=> {
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product'){
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    };
+});
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('server is listening on port 8000...')
+})
