08ba4c8b5fc0582be2a861b202555094b17681d6
mosesbenjamin <mavewreek@gmail.com>
2020-11-20 02:12:51 +0100

    API..

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,1 +1,28 @@
-console.log("Hello world")
+const http = require('http')
+const fs = require('fs')
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+
+const server = http.createServer((req, res)=> {
+    const pathname = req.url
+
+    if(pathname === '/' || pathname === '/overview'){
+        res.end('This is the OVERVIEW')
+    } else if(pathname === '/product'){
+        res.end('This is the PRODUCT')
+    } else if(pathname === '/api'){ 
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(data)
+    }else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html',
+            'my-own-header': 'hello-there!'
+        })
+        res.end('<h1>Page not found!')
+    }
+})
+
+server.listen(8000, '127.0.0.1', ()=> {
+    console.log('Server is up on port 8000')
+})
