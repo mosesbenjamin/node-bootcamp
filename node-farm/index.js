70ec1d7e68f39f3343d7160b6357e283f77bc061
mosesbenjamin <mavewreek@gmail.com>
2020-11-20 03:24:53 +0100

    Refactor to use module.

--- node-farm/index.js
+++ node-farm/index.js
@@ -2,6 +2,8 @@ const http = require('http')
 const fs = require('fs')
 const url = require('url')
 
+const replaceTemplate = require('./modules/replaceTemplate')
+
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
@@ -9,21 +11,6 @@ const dataObj = JSON.parse(data)
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
-
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%NAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%ID%}/g, product.id)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    return output
-    
-}
 
 const server = http.createServer((req, res)=> {
 
