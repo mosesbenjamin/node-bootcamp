5f0b7a1ff336e57a0aa11564718b9ab8f642b7cf
mosesbenjamin <mavewreek@gmail.com>
2020-11-21 14:04:32 +0100

    Handling POST requests

--- natours/app.js
+++ natours/app.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 
 const app = express()
 
+app.use(express.json())
+
 // app.get('/', (req, res)=> {
 //     res.json({ message: 'Hello from the server side', app: 'Natours' })
 // })
@@ -20,6 +22,21 @@ app.get('/api/v1/tours', (req, res)=> {
         data: {
             tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res)=> {
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
