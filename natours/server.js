f7aac2bc7f1bf517afef8ffac992325ceb039d07
mosesbenjamin <mavewreek@gmail.com>
2020-11-23 00:02:22 +0100

    Connecting our database with express

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' })
+const colors = require('colors')
 
 const app = require('./app')
+dotenv.config({ path: './config.env' })
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DATABASE,  {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        })
+        console.log( `DB connection successful. \n${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.error(error);
+        process.exit(1)
+    }
+}
+
+
+connectDB()
 
 
 // START SERVER
