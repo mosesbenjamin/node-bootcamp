68409af2a0070b26a15a00e61a4aa4459acbdb11
mosesbenjamin <mavewreek@gmail.com>
2020-11-23 00:23:15 +0100

    Creating documents and testing the model

--- natours/server.js
+++ natours/server.js
@@ -39,6 +39,19 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema)
 
+const testTour = new Tour({
+    name: 'The Forest Hiker2',
+    rating: 4.7,
+    price: 497
+})
+
+testTour.save().then(doc => {
+    console.log(doc)
+}).catch(error=> {
+    console.error(error)
+})
+
+
 // START SERVER
 const port = process.env.PORT || 3000
 app.listen(port, ()=> {
