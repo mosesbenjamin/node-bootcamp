23ac4e768d62547569d47b4bbe89f5a5866ee032
mosesbenjamin <mavewreek@gmail.com>
2020-11-25 13:26:18 +0100

    Reading documents

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,14 +1,20 @@
 const Tour = require('../models/tourModel')
 
-const getAllTours = (req, res)=> {
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    })
+const getAllTours = async (req, res)=> {
+    try {
+        const tours = await Tour.find()
+
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {tours}
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error
+        })
+    }
 }
 
 const createTour = async (req, res)=> {
@@ -42,14 +48,19 @@ const deleteTour = (req, res)=> {
     })
 }
 
-const getTour = (req, res)=> {
-
-    res.status(200).json({
-        status: 'success',
-        // data: {
-        //     tour
-        // }
-    })
+const getTour = async (req, res)=> {
+    try {
+        const tour = await Tour.findById(req.params.id)
+        res.status(200).json({
+            status: 'success',
+            data: {tour}
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error
+        })
+    }
 }
 
 module.exports = { 
