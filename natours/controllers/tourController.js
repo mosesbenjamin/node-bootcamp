2f257e97e082f2d98346114f2e2d3ceacaa112a2
mosesbenjamin <mavewreek@gmail.com>
2020-11-25 13:38:53 +0100

    Updating documents

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -32,13 +32,23 @@ const createTour = async (req, res)=> {
        }
 }
 
-const updateTour =(req, res)=> {
-    res.status(200).json({
-        status: 'success',
-        // data: {
-        //     tour: '<Updated tour here..>'
-        // }
-    })
+const updateTour = async (req, res)=> {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        res.status(200).json({
+            status: 'success',
+            data: {tour}
+        })
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error
+        })
+   }
 }
 
 const deleteTour = (req, res)=> {
