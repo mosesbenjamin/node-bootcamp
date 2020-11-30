f8937f52e05b78c850b8ace53777aabc27a57854
mosesbenjamin <mavewreek@gmail.com>
2020-12-01 00:50:01 +0100

    Aggregation pipeline- Matching and grouping

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -93,11 +93,50 @@ const getTour = async (req, res)=> {
     }
 }
 
+const getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    _id: {  $toUpper: '$difficulty'},
+                    // _id: '$difficulty',
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' }
+                }
+            },
+            {
+                $sort: { avgPrice: 1 }
+            }
+            // {
+            //     $match: { _id: { $ne: 'EASY' } }
+            // }
+        ])
+
+        res.status(200).json({
+            status: 'success',
+            data: {stats}
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error
+        })
+    }
+}
+
 module.exports = { 
     getAllTours, 
     getTour, 
     updateTour, 
     deleteTour, 
     createTour,
-    aliasTopTours 
+    aliasTopTours,
+    getTourStats 
 }
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -6,7 +6,8 @@ const {
     createTour, 
     updateTour, 
     deleteTour,
-    aliasTopTours
+    aliasTopTours,
+    getTourStats
 } = require('../controllers/tourController')
 
 const router = express.Router()
@@ -14,6 +15,8 @@ const router = express.Router()
 // router.param('id', checkID)
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
+
+router.route('/tour-stats').get(getTourStats)
 
 router.route('/').get(getAllTours).post(createTour)
 
