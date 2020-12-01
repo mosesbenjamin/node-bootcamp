50028b63a2915616ed5fd15806c93454eeae268d
mosesbenjamin <mavewreek@gmail.com>
2020-12-01 01:18:42 +0100

    Aggregation pipeline- Unwinding and projecting

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -131,6 +131,57 @@ const getTourStats = async (req, res) => {
     }
 }
 
+const getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1 // 2021
+
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates'
+            },
+            {
+                $match: {
+                    startDates: { 
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: '$name' }
+                }
+            },
+            {
+                $addFields: { month: '$_id' }
+            },
+            {
+                $project: {
+                    _id: 0
+                }
+            },
+            {
+                $sort: { numTourStarts: -1 }
+            },
+            {
+                $limit: 12
+            }
+        ])
+
+        res.status(200).json({
+            status: 'success',
+            data: {plan}
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error
+        })
+    }
+}
+
 module.exports = { 
     getAllTours, 
     getTour, 
@@ -138,5 +189,6 @@ module.exports = {
     deleteTour, 
     createTour,
     aliasTopTours,
-    getTourStats 
+    getTourStats,
+    getMonthlyPlan 
 }
