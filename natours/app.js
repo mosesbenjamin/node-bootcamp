15fef8222c741ea161624f32988c66f957e74439
mosesbenjamin <mavewreek@gmail.com>
2020-12-06 00:53:49 +0100

    Handling unhandled routes

--- natours/app.js
+++ natours/app.js
@@ -15,10 +15,6 @@ if(process.env.NODE_ENV === 'development'){
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware')
-    next()
-})
 
 app.use((req, res, next)=> {
     req.requestTime = new Date().toISOString()
@@ -29,5 +25,13 @@ app.use((req, res, next)=> {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+    next()
+})
 
 module.exports = app
