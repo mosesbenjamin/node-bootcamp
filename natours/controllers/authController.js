88871bdf0c568c336ae666c09af513fc38ecaa30
mosesbenjamin <mavewreek@gmail.com>
2020-12-07 02:54:36 +0100

    Logging in users

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken')
 
+const AppError = require('../utils/appError')
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
 
 const signup = catchAsync(async(req, res, next) => {
     const user = await User.create({
@@ -11,9 +18,7 @@ const signup = catchAsync(async(req, res, next) => {
         passwordConfirm: req.body.passwordConfirm
     })
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
+    const token = signToken(user._id )
 
     res.status(201).json({
         status: 'success',
@@ -22,6 +27,30 @@ const signup = catchAsync(async(req, res, next) => {
     })
 })
 
+const login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body
+
+    // 1) Check if email and password exist
+    if(!email || !password){
+        return next(new AppError('Please provide email and password', 400))
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user || !(await user.correctPassword(password, user.password))){
+        return next(new AppError('Incorrect email or password', 401))
+    }
+
+    // 3) If everything is ok, send token to client
+    const token = signToken(user._id)
+    res.status(200).json({
+        status: 'success',
+        token
+    })
+})
+
 module.exports = {
-    signup
+    signup,
+    login
 }
