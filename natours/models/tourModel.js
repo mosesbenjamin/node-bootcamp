46213ef880658e919bc91f6cd5899b0bcec33404
mosesbenjamin <mavewreek@gmail.com>
2020-12-14 12:25:15 +0100

    Modelling tour guides- Embedding

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const slugify = require('slugify')
 const validator = require('validator')
 
+const User = require('./userModel')
+
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -100,7 +102,8 @@ const tourSchema = new mongoose.Schema({
             description: String,
             day: Number
         }
-    ]
+    ],
+    guides: Array
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -115,6 +118,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, {lower: true})
     next()
+})
+
+tourSchema.pre('save', async function (next){
+    const guidesPromises = this.guides.map( async id => await User.findById(id) )
+    this.guides = await Promise.all(guidesPromises)
+    next();
 })
 
 // tourSchema.pre('save', function (next) {
