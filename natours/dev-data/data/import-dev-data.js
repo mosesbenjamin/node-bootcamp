9e4b0b1b690cc9d03009589d613b00c6ff3a0188
mosesbenjamin <mavewreek@gmail.com>
2020-12-14 12:07:06 +0100

    Modelling locations- Geospatial data

--- natours/dev-data/data/import-dev-data.js
+++ natours/dev-data/data/import-dev-data.js
@@ -24,7 +24,7 @@ connectDB()
 
 // READ JSON FILE
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 
 // IMPORT DATA INTO DATABASE
