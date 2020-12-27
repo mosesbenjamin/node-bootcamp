d9c28b1aa8ef3ba0df6490fcb12fe37b0c7a90d8
mosesbenjamin <mavewreek@gmail.com>
2020-12-27 15:30:34 +0100

    Building the tour overview- I

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -1,9 +1,19 @@
+const Tour = require('../models/tourModel')
+const catchAsync = require('../utils/catchAsync')
 
-const getOverview = (req, res) => {
+const getOverview = catchAsync( async (req, res) => {
+    // 1) Get tour data from collection
+    const tours = await Tour.find()
+
+    // 2) Build template
+
+    // 3) Render the template using tour data from 1)
+
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     })
-}
+})
 
 const getTour = (req, res) => {
     res.status(200).render('tour', {
