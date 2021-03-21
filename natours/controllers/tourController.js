c8f1f95d43a83f0da5ab17729747455ed81dd9bb
mosesbenjamin <mavewreek@gmail.com>
2021-03-21 22:05:14 +0100

    uploading multiple images

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,171 +1,212 @@
-const Tour = require('../models/tourModel')
-const AppError = require('../utils/appError')
-const catchAsync = require('../utils/catchAsync')
-const factory = require('./handlerFactory')
+const multer = require('multer');
+const sharp = require('sharp');
+
+const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+const uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+// upload.single('image'), req.file
+// upload.array('images', 5), req.files
+
+const resizeTourImages = (req, res, next) => {
+  console.log(req.files);
+  next();
+};
 
 const aliasTopTours = async (req, res, next) => {
-    req.query.limit = '5'
-    req.query.sort = '-ratingsAverage,price'
-    req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
-    next()
-}
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 const getTourStats = catchAsync(async (req, res, next) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } },
+    },
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' },
+        // _id: '$difficulty',
+        numTours: { $sum: 1 },
+        numRatings: { $sum: '$ratingsQuantity' },
+        avgRating: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' },
+      },
+    },
+    {
+      $sort: { avgPrice: 1 },
+    },
+    // {
+    //     $match: { _id: { $ne: 'EASY' } }
+    // }
+  ]);
 
-    const stats = await Tour.aggregate([
-        {
-            $match: { ratingsAverage: { $gte: 4.5 } }
-        },
-        {
-            $group: {
-                _id: {  $toUpper: '$difficulty'},
-                // _id: '$difficulty',
-                numTours: { $sum: 1 },
-                numRatings: { $sum: '$ratingsQuantity' },
-                avgRating: { $avg: '$ratingsAverage' },
-                avgPrice: { $avg: '$price' },
-                minPrice: { $min: '$price' },
-                maxPrice: { $max: '$price' }
-            }
-        },
-        {
-            $sort: { avgPrice: 1 }
-        }
-        // {
-        //     $match: { _id: { $ne: 'EASY' } }
-        // }
-    ])
-
-    res.status(200).json({
-        status: 'success',
-        data: {stats}
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    data: { stats },
+  });
+});
 
 const getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1; // 2021
 
-    const year = req.params.year * 1 // 2021
-
-    const plan = await Tour.aggregate([
-        {
-            $unwind: '$startDates'
+  const plan = await Tour.aggregate([
+    {
+      $unwind: '$startDates',
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`),
         },
-        {
-            $match: {
-                startDates: { 
-                    $gte: new Date(`${year}-01-01`),
-                    $lte: new Date(`${year}-12-31`),
-                }
-            }
+      },
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' },
+      },
+    },
+    {
+      $addFields: { month: '$_id' },
+    },
+    {
+      $project: {
+        _id: 0,
+      },
+    },
+    {
+      $sort: { numTourStarts: -1 },
+    },
+    {
+      $limit: 12,
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: { plan },
+  });
+});
+// /tours-within/distance/300/latlng/34.239557,-118.480441/unit/mi
+const getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the ffl format: lat,lng',
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
+    },
+  });
+});
+
+const getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the ffl format: lat,lng',
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
         },
-        {
-            $group: {
-                _id: { $month: '$startDates' },
-                numTourStarts: { $sum: 1 },
-                tours: { $push: '$name' }
-            }
-        },
-        {
-            $addFields: { month: '$_id' }
-        },
-        {
-            $project: {
-                _id: 0
-            }
-        },
-        {
-            $sort: { numTourStarts: -1 }
-        },
-        {
-            $limit: 12
-        }
-    ])
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
 
-    res.status(200).json({
-        status: 'success',
-        data: {plan}
-    })
-})
-// /tours-within/distance/300/latlng/34.239557,-118.480441/unit/mi 
-const getToursWithin = catchAsync (async(req, res, next) => {
-    const { distance, latlng, unit } = req.params
-    const [lat, lng] = latlng.split(',')
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
 
-    const radius = unit === 'mi'? distance / 3963.2 : distance / 6378.1
+const getAllTours = factory.getAll(Tour);
 
-    if(!lat || !lng) {
-        next(new AppError('Please provide latitude and longitude in the ffl format: lat,lng', 400))
-    }
+const createTour = factory.createOne(Tour);
 
-    const tours = await Tour.find({ 
-        startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } } 
-    })
+const updateTour = factory.updateOne(Tour);
 
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            data: tours
-        }
-    })
-})
+const deleteTour = factory.deleteOne(Tour);
 
-const getDistances = catchAsync(async(req, res, next) => {
-    const { latlng, unit } = req.params
-    const [lat, lng] = latlng.split(',')
+const getTour = factory.getOne(Tour, { path: 'reviews' });
 
-    const multiplier = unit === 'mi' ? 0.000621371 : 0.001
-
-    if(!lat || !lng) {
-        next(new AppError('Please provide latitude and longitude in the ffl format: lat,lng', 400))
-    }
-
-    const distances = await Tour.aggregate([
-        {
-            $geoNear: {
-                near: {
-                    type: 'Point',
-                    coordinates: [lng * 1, lat * 1]
-                },
-                distanceField: 'distance',
-                distanceMultiplier: multiplier
-            }
-        },
-        {
-            $project: {
-                distance: 1,
-                name: 1
-            }
-        }
-    ])
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            data: distances
-        }
-    })
-})
-
-const getAllTours = factory.getAll(Tour)
-
-const createTour = factory.createOne(Tour)
-
-const updateTour = factory.updateOne(Tour)
-
-const deleteTour = factory.deleteOne(Tour)
-
-const getTour = factory.getOne(Tour, { path: 'reviews' })
-
-module.exports = { 
-    getAllTours, 
-    getTour, 
-    updateTour, 
-    deleteTour, 
-    createTour,
-    aliasTopTours,
-    getTourStats,
-    getMonthlyPlan,
-    getToursWithin,
-    getDistances
-}
+module.exports = {
+  getAllTours,
+  getTour,
+  updateTour,
+  deleteTour,
+  createTour,
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
+  getToursWithin,
+  getDistances,
+  uploadTourImages,
+  resizeTourImages,
+};
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -1,52 +1,60 @@
-const express = require('express')
-
-const { 
-    getAllTours, 
-    getTour, 
-    createTour, 
-    updateTour, 
-    deleteTour,
-    aliasTopTours,
-    getTourStats,
-    getMonthlyPlan,
-    getToursWithin,
-    getDistances
-} = require('../controllers/tourController')
+const express = require('express');
 
 const {
-    protect,
-    restrictTo
-} = require('../controllers/authController')
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
+  getToursWithin,
+  getDistances,
+  uploadTourImages,
+  resizeTourImages,
+} = require('../controllers/tourController');
 
-const reviewRouter = require('../routes/reviewRoutes')
+const { protect, restrictTo } = require('../controllers/authController');
 
-const router = express.Router()
+const reviewRouter = require('../routes/reviewRoutes');
+
+const router = express.Router();
 
 // router.param('id', checkID)
 
-router.use('/:tourId/reviews', reviewRouter)
+router.use('/:tourId/reviews', reviewRouter);
 
-router.route('/top-5-cheap')
-    .get(aliasTopTours, getAllTours)
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
-router.route('/tour-stats')
-    .get(getTourStats)
+router.route('/tour-stats').get(getTourStats);
 
-router.route('/monthly-plan/:year')
-    .get(protect, restrictTo('admin', 'lead-guide', 'guide'),getMonthlyPlan)
+router
+  .route('/monthly-plan/:year')
+  .get(protect, restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan);
 
 // /tours-within?distance=300&latlng=-40,30&unit=mi       Using query strings
-router.route('/tours-within/:distance/latlng/:latlng/unit/:unit').get(getToursWithin)
+router
+  .route('/tours-within/:distance/latlng/:latlng/unit/:unit')
+  .get(getToursWithin);
 
-router.route('/distances/:latlng/unit/:unit').get(getDistances)
+router.route('/distances/:latlng/unit/:unit').get(getDistances);
 
-router.route('/')
-    .get(getAllTours)
-    .post(protect, restrictTo('admin', 'lead-guide'), createTour)
+router
+  .route('/')
+  .get(getAllTours)
+  .post(protect, restrictTo('admin', 'lead-guide'), createTour);
 
-router.route('/:id')
-    .get(getTour)
-    .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
-    .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour)
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(
+    protect,
+    restrictTo('admin', 'lead-guide'),
+    uploadTourImages,
+    resizeTourImages,
+    updateTour
+  )
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
-module.exports = router
+module.exports = router;
