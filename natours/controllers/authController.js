36ab67b08623428094bfecbe5ac4a9d1c5bb7dc4
mosesbenjamin <mavewreek@gmail.com>
2021-03-21 23:23:48 +0100

    sending password reset emails

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -137,11 +137,7 @@ const forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a patch request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email`;
 
   try {
-    // await sendEmail({
-    //     email: user.email,
-    //     subject: 'Your password reset token (valid for 10mins)',
-    //     message
-    // })
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
